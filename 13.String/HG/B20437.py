717127f6d03480fd634e589ac9b2e8fc3a90c243
geun9716 <crintop@gmail.com>
2022-04-18 13:36:45 +0900

    [B20437] 문자열 게임2 - String

--- 13.String/HG/B20437.py
+++ 13.String/HG/B20437.py
@@ -1,7 +1,30 @@
 import sys
 input = sys.stdin.readline
+from collections import defaultdict
 
 for _ in range(int(input())):
     W = input().strip()
     K = int(input())
-    print(W, K)
+    alpIdx = defaultdict(list)
+    candidate = set()
+    for idx, ch in enumerate(W):
+        alpIdx[ch].append(idx)
+        if len(alpIdx[ch]) >= K:
+            candidate.add(ch)
+
+    if not candidate:
+        print(-1)
+        continue
+
+    minValue = float('inf')
+    maxValue = 0
+
+    for ch in candidate:
+        arr = alpIdx[ch]
+        size = K-1
+        for i in range(len(arr)- size):
+            length = (arr[i+size]+1 - arr[i])
+            minValue = min(length, minValue)
+            maxValue = max(length, maxValue)
+
+    print(minValue, maxValue)
