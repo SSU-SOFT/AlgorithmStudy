c8c640eeb28ee38bf85fe2c5ef0c6780dc2fc3f0
geun9716 <crintop@gmail.com>
2022-05-05 12:24:09 +0900

    [B2141] 우체국 - 미완

--- 05.Greedy/HG/B2141.py
+++ 05.Greedy/HG/B2141.py
@@ -16,20 +16,18 @@ input = sys.stdin.readline
 N = int(input())
 X = [0] * (N+1)
 L = 0
+mid = 0
 location = 1
 R = 0
 for _ in range(N):
     i, a = map(int, input().split())
     X[i] = a
     R += abs(i-location) * a # 새로 들어온 사람 거리 합
-    if L < R: # 왼쪽보다 크다면 location을 키워 최대한 맞춤.
-        while L < R:
-            location += 1
-            L, R = 0, 0
-            for j in range(1, location):
-                L += abs(j-location) * X[j]
-            for k in range(location, len(X)):
-                R += abs(k-location) * X[k]
+    tmpL = L + mid
+    if tmpL < R:
+        location += 1
+        L = tmpL
+        
     #         print(L, R)
     # print(L, location, R)
 print(location)
